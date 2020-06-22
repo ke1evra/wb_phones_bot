32f3c553ad9ae0511703a89cded9430f0f8a474f
ke1eva <dolgov.konstantin@gmail.com>
2020-06-22 17:22:51 +0300

    get -> post

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const chatid = '-449604345';
 const moment = require('moment'); // require
 moment.locale('ru');
 
-app.get('/wbphones', function (req, res) {
+app.post('/wbphones', function (req, res) {
     console.log('Запрос на /wbphones');
     const q = req.query;
     const b = req.body;
     console.log(JSON.stringify(b));
-    const message = `sms от ${b.src} на ${b.dst} (${moment().format('LLL')})\n---------------------\n${b.body}`;
+    const message = `sms от ${b.src} на ${b.dst} (${moment().format('DD.MM.YYYY HH:mm:ss')})\n---------------------\n${b.body}`;
     bot.sendMessage(chatid, message).then(() => {
         console.log(`сообщение ${message} успешно отправлено в чат ${chatid}`);
         res.status(200).end()
