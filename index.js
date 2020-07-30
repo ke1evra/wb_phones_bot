99ce14aac46b3ac9c70e24682b76692aaf25c2ea
ke1eva <dolgov.konstantin@gmail.com>
2020-07-30 19:23:18 +0300

    minor fix

--- index.js
+++ index.js
@@ -15,23 +15,6 @@ app.use('/informer', informerRouter);
 app.use('/wbphones', wbPhonesRouter);
 
 
-app.post('/wbphones', function (req, res) {
-    try{
-        console.log(`Запрос на /wbphones (${moment().format('DD.MM.YYYY HH:mm:ss')})`);
-        const b = req.body;
-        console.log(JSON.stringify(b));
-        const message = `sms от ${b.src} на ${b.dst} (${moment().format('DD.MM.YYYY HH:mm:ss')})\n---------------------\n${b.body}`;
-        bot.sendMessage(chatid, message).then(() => {
-            console.log(`сообщение ${message} успешно отправлено в чат ${chatid}`);
-            res.status(200).end()
-        }).catch(e => {
-            console.log(e);
-        });
-    }catch(e){
-        console.log(e);
-    }
-});
-
 app.listen(port, function () {
     console.log(`Сервер запущен на ${port} порту`);
 });
