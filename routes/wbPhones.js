ed32497fb157bfb56f52abe3a49fc1dba439b27e
ke1eva <dolgov.konstantin@gmail.com>
2020-07-30 19:26:14 +0300

    minor fix

--- routes/wbPhones.js
+++ routes/wbPhones.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const actions = require('../botActions');
 const moment = require('moment');
 
-router.post('/wbphones', function (req, res) {
+router.post('/', function (req, res) {
     try{
         console.log(`Запрос на /wbphones (${moment().format('DD.MM.YYYY HH:mm:ss')})`);
         actions.sendMessageFromZebra(req.body)
