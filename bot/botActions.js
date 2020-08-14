b3234c71686d659cdc87260c1afbdd720ec15dce
ke1eva <dolgov.konstantin@gmail.com>
2020-08-14 18:24:04 +0300

    test command

--- bot/botActions.js
+++ bot/botActions.js
@@ -99,4 +99,16 @@ bots.vkostume_informer.on('callback_query', function (msg) {
     });
 });
 
+bots.vkostume_informer.onText(/\/echo (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+
+    const resp = match[1]; // the captured "whatever"
+
+    // send back the matched "whatever" to the chat
+    bots.vkostume_informer.sendMessage(chats.me, resp)
+        .then(()=> console.log('ok'));
+});
+
 module.exports = methods;
