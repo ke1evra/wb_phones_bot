d7e148d9f6f6ed77fe8747e94489b85ef9a35e9d
kortess1999 <kortess1999@mail.ru>
2021-01-13 12:42:01 +0300

    Added method cleanUp with 50 rows max.

--- googleApi/googleApiManager.js
+++ googleApi/googleApiManager.js
@@ -5,11 +5,26 @@ const API = {
     async login(){
         return await doc.useServiceAccountAuth(require('./dolgovapi-955301a7af9e.json'));
     },
+    ///Чистка излишков сообщений
+    async cleanUp(sheet)
+    {
+        const rowsNumber=50;//максимальное число строк
+        while(sheet.rowCount>rowsNumber)
+        {
+            sheet.rows[1].delete();
+            for(let i=1;i<sheet.rowCount;i++)
+            {
+                sheet.rows[i]=sheet.rows[i+1];
+                sheet.rows[i].save();
+            }
+        }
+    },
     async addRow(chat_id, message_id, message){
         await this.login();
         await doc.loadInfo();
         const sheet = doc.sheetsByIndex[0];
         await sheet.addRow({ chat_id, message_id, message });
+        await this.cleanUp(sheet);
     },
     async getMessages(){
         await this.login();
