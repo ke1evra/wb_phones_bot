7b8cce5c21e7958b89f2c6e25250fc030409c36e
kortess <kortess1999@mail.ru>
2021-03-02 17:06:10 +0300

    debug

--- bot/botActions.js
+++ bot/botActions.js
@@ -121,7 +121,7 @@ const methods = {
           .sendMessage(chat_id, message)
           .then((msg) => {
             console.log("message=",message);
-            console.log(`сообщение (id: ${msg.message_id})${message.length>80?message.splice(0,80)+'...':message} успешно отправлено в чат (${chat_id})`
+            console.log(`сообщение (id: ${msg.message_id})${message.length>80?message.substr(0,80)+'...':message} успешно отправлено в чат (${chat_id})`
             );
             return msg;
           }).catch((e) => {
