1d7e13c75c8bd6dd4965c75c84c216bd9aa026ce
kortess <kortess1999@mail.ru>
2021-03-16 16:49:16 +0300

    Добавил новые чаты

--- bot/chatList.js
+++ bot/chatList.js
@@ -3,5 +3,8 @@ module.exports = {
   manager: '-1001317263445',
   me: '502738350',
   marketplaces: '-237208504',
-  reports:'-517454608'
+  reports_hour:'-1001350952191',
+  reports_day:'-1001271691277',
+  reports_week:'-517454608',
+  reports_month:'-1001272866729'
 };
