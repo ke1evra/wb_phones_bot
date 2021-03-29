301a8cf96df2d618861ec44bfdf42f9956978f91
E123 <E123wh@mail.ru>
2021-03-29 07:02:53 +0300

    добавляю заказы по менеджерам

--- analytics/menu.js
+++ analytics/menu.js
@@ -191,7 +191,7 @@ class Menu {
         const data = await API.getManagersCalls(fields.days);
         //console.log(data.data.data1);
         //console.log(data.data.incomingByNumber);
-        let message = '`Менеджеры:\n';
+        let message = '```Менеджеры:\n';
         const menu = [];
         //console.log(data.data["data1"]);
 
@@ -366,8 +366,8 @@ class Menu {
                 message += `\n\nЗанятость: ${messageData[manager]['basic_info']['business']}%`
             }
         }
-        message=message.replace(/\n/g,"`\n`")
-        message+='`'
+        message=message.replace(/\n/g,"```\n```")
+        message+='```'
         let options = {
             reply_markup: JSON.stringify({
                 inline_keyboard: [
