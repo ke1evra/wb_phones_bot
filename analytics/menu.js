870be93cb626222cf2f5dd6262e9bbfca3b7d942
E123 <E123wh@mail.ru>
2021-04-06 03:13:09 +0300

    отладка отправки больших сообщений(4000 символов?)

--- analytics/menu.js
+++ analytics/menu.js
@@ -341,7 +341,7 @@ class Menu {
 
         const width = 34
 
-        let message = `—— Отчет по менеджерам —${'—'.repeat(width - 24)}\n\n`;
+        let message = `— Отчет по менеджерам —${'—'.repeat(width - 23)}\n\n`;
         message += `Звонков совершено: ${messageData.all_managers.calls.basic_info.total_calls_count}\n` +
             `Ср. продолжительность звонка: ${messageData.all_managers.calls.basic_info.avg_call_duration}\n` +
             `Ср. время ответа: ${messageData.all_managers.calls.incoming_calls_info.avg_time_to_answer}\n` +
@@ -409,7 +409,7 @@ class Menu {
                 }
             }
         }
-        message=message.match(/[\s\S]{1,4090}/g).map(e=>{
+        message=message.match(/[\s\S]{1,3997}/g).map(e=>{
             return `\`\`\`${e}\`\`\``
         }).join('')
         console.log(message)
